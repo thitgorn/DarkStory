b0e7da1b4fcd37c810d3b008561cf3aa7b8b4bf6
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-05-23 12:51:01 +0700

    change demo2 show text

--- app/src/main/java/com/example/thitiwat/darkstory/model/repository/MockupRepository.kt
+++ app/src/main/java/com/example/thitiwat/darkstory/model/repository/MockupRepository.kt
@@ -7,7 +7,7 @@ class MockupRepository: Repository() {
     override fun loadQuestion() {
         val mockQuestion = ArrayList<Question>()
         mockQuestion.add(Question(1, "mockdemo1" , arrayOf("demo","test","example") , "demo1" , "answer : demo1 ,this is mock data for question one it is not everything else:: !! you are just went offline mode"))
-        mockQuestion.add(Question(2, "mockdemo2" , arrayOf("demo","test","example") , "demo2" , "answer : demo1 ,this is mock data for question two it is not everything else:: !! you are just went offline mode"))
+        mockQuestion.add(Question(2, "mockdemo2" , arrayOf("demo","test","example") , "demo2" , "answer : demo2 ,this is mock data for question two it is not everything else:: !! you are just went offline mode"))
         super.setQuestion(mockQuestion)
     }
 
